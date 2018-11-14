ab9f8f9c52a83ee5405491bba21654a8979b8a81
Anmol Dua <101983924@student.swin.edu.au>
2018-11-15 03:57:21 +1100

    Added  base classes for QuickReply for #16

---
+++ src/LineBot/Messages/Shared/QuickReply.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Line
+{
+    /// <summary>
+    /// Quick reply for message.
+    /// </summary>
+    /// <seealso cref="Line.ISendMessage" />
+    public sealed class QuickReply : ISendMessage
+    {
+        [JsonProperty("items")]
+        [JsonConverter(typeof(EnumConverter<ActionType>))]
+        private IAction[] Actions { get; set; }
+
+        void ISendMessage.Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/LineBot.Tests/Messages/shared/QuickReplyTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Line.Tests
+{
+    [TestClass]
+    public sealed class QuickReplyTests
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+        }
+    }
+}
